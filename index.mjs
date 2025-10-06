30a5dfdea07ae530d0dc2cde6d191a34027f373b
esha-abid-dot <eshaabid62@gmail.com>
2025-10-06 17:35:59 +0500

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,34 +1,7 @@
 export const handler = async (event) => {
-    // Method nikal lo (HTTP API ke liye yeh property hoti hai)
-    const method = event.requestContext.http.method;
-
-    if (method === "GET") {
-        return {
-            statusCode: 200,
-            body: JSON.stringify({
-                message: "You called a GET request!"
-            }),
-        };
-    } 
-    else if (method === "POST") {
-        // POST body parse kar lo
-        const data = JSON.parse(event.body || "{}");
-
-        return {
-            statusCode: 200,
-            body: JSON.stringify({
-                message: "You called a POST request!",
-                yourData: data
-            }),
-        };
-    } 
-    else {
-        return {
-            statusCode: 405,
-            body: JSON.stringify({
-                error: "Method Not Allowed"
-            }),
-        };
-    }
+  return {
+    statusCode: 200,
+    body: JSON.stringify('Hello from updated Lambda!'),
+  };
 };
 
